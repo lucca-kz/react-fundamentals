ad4af5b5b0c737f138f4775eec88fc99c6fd8114
Lucca Kazlauskas Zaccaria <zaccaria.kaz@gmail.com>
2025-04-29 15:20:07 -0300

    Create my first component and use in `App.jsx`

---
+++ my-first-app/src/App.jsx
@@ -0,0 +1,16 @@
+import { useState } from "react";
+import reactLogo from "./assets/react.svg";
+import viteLogo from "/vite.svg";
+import FirstComponent from "./components/FirstComponent.jsx";
+import "./App.css";
+
+function App() {
+  return (
+    <>
+      <h1>Fundamentos do react</h1>
+      <FirstComponent />
+    </>
+  );
+}
+
+export default App;
